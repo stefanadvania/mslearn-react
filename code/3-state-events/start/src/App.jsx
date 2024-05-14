bcbd8daa1d5395b25eb1a4a38317105c7083dade
stefanadvania <151943095+stefanadvania@users.noreply.github.com>
2024-05-14 13:19:21 +0200

    close course

--- code/3-state-events/start/src/App.jsx
+++ code/3-state-events/start/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
             reviews: 20
         },
         ingredients: [
-            { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
+            { name: '3 potatoes, cut into 1/2 pieces', prepared: false },
             { name: '4 Tbsp butter', prepared: false },
             { name: '1/8 cup heavy cream', prepared: false },
             { name: 'Salt', prepared: true },
@@ -20,22 +20,35 @@ function App() {
     };
 
     // TODO: Create recipe state
+    const [ recipe, setRecipe ] = useState(initialRecipe);
 
     // TODO: Add new state property
+    const [ prepared, setPrepared ] = useState(false);
 
     // TODO: Create ingredientClick event listener
+    function ingredientClick(index) {
+        const updateRecipe = { ... recipe };
+        updateRecipe.ingredients[index].prepared = !updateRecipe.ingredients[index].prepared;
+        setRecipe(updateRecipe);
+    }
 
     // TODO: Add the effect hook
+    useEffect(() => {
+        setPrepared(recipe.ingredients.every(i => i.prepared));
+    }, [recipe]);
 
     return (
         <article>
             <h1>Recipe Manager</h1>
 
             {/* TODO: Pass recipe metadata to RecipeTitle */}
+            <RecipeTitle title={recipe.title} feedback={recipe.feedback} />
 
             {/* TODO: Pass ingredients and event listener to IngredientList */}
+            <IngredientList ingredients={recipe.ingredients} onClick={ ingredientClick } />
 
             {/* TODO: Add the prep work display */}
+            { prepared ? <h2>Prep work done!</h2> : <h2>Just keep chopping.</h2> }
         </article>
     )
 }
